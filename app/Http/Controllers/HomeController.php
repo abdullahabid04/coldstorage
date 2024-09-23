226e3f2d888c0133b76314ab37d1d898fc3a01d3
Abdullah Abid <abdrps2004@gmail.com>
2024-09-23 12:26:49 +0500

    Working on some new APIs for device management, models and migrations are created, now working on controllers.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,7 +38,7 @@ class HomeController extends Controller
                 return view('dashboard.admin');
             } else {
 
-                if (Auth::user()->role->id === 3) {
+                if (Auth::user()->role->id === 4) {
                     $stores = auth()->user()->stores()->with(['areas' => function($query) {
                         $query->whereHas('users', function ($query) {
                             $query->where('user_id', auth()->id());
